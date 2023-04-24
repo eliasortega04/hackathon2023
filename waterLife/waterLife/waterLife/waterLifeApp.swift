7094a6278830eeb942077b0aeeaf0be434d66286
eliasortega04 <60422749+eliasortega04@users.noreply.github.com>
2023-04-24 11:44:13 -0600

    Add files via upload

---
+++ waterLife/waterLife/waterLife/waterLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterLifeApp.swift
+//  waterLife
+//
+//  Created by CEDAM 14 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct waterLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
