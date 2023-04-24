4c337788f259d9962d0138f00beb674d90b96d0e
AbbyVillsM <94766276+AbbyVillsM@users.noreply.github.com>
2023-04-24 17:41:55 -0600

    Add files via upload

---
+++ hackathon2023/devMasters/devMasters/aguaViewController.swift
@@ -0,0 +1,32 @@
+//
+//  aguaViewController.swift
+//  devMasters
+//
+//  Created by CEDAM 15  on 24/04/23.
+//
+
+import UIKit
+
+class aguaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBSegueAction func btnTortuga(_ coder: NSCoder) -> aguaViewController? {
+        return <#aguaViewController(coder: coder)#>
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ hackathon2023/devMasters/devMasters/tierraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  tierraViewController.swift
+//  devMasters
+//
+//  Created by CEDAM 15  on 24/04/23.
+//
+
+import UIKit
+
+class tierraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
